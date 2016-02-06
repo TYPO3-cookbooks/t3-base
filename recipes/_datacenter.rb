a8446846fc125040e5ef72f85552ea9dbc5ef7bc
Steffen Gebert <steffen.gebert@typo3.org>
2016-02-06 23:33:35 +0100

    Add data-bag-driven data center detection and configuration
    The `t3-base::_datacenter` recipe detects, if we are running in a production
    data center. It therefore searches the `datacenters` data bag for entries
    including the (physical) server that we are running on.
    
    If it is detected that we are running in the data center, the
    `t3-base::_production` recipe is automatically included.
    This applies production-only configuration (chef-client config, backup,
    monitoring). **Make sure that the things
    added here have no impact on other configuration!**
    
    Such data bag entry can define data center specific **attributes** and
    **cookbooks** (for a server `test.vagrant` in the `test` data center and its
    VMs):
    
    ```
    {
      "name": "test",
      "servers": [
        "test.vagrant"
      ],
      "attributes": {
        "postfix": {
          "main": {
            "relayhost": "test.example.com"
          }
        }
      },
      "cookbooks": [
        "special-cookbook-for-dc-test"
      ]
    }
    ```

---
+++ recipes/_datacenter.rb
@@ -0,0 +1,73 @@
+#
+# Cookbook Name:: t3-base
+# Recipe:: _datacenter
+#
+# Copyright 2016, TYPO3 Association
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+::Chef::Recipe.send(:include, Typo3::Base::Node)
+::Chef::Recipe.send(:include, Typo3::Base::Recipe)
+
+#############################
+# who's my host?
+#############################
+if !node[:virtualization][:masterserver].nil?
+  physical_server = node[:virtualization][:masterserver]
+else
+  physical_server = node[:fqdn]
+end
+Chef::Log.info "Physical server: #{physical_server}"
+
+#############################
+# search datacenters data bag
+#############################
+dc_data = search(:datacenters, "servers:#{physical_server}")
+
+if dc_data.length == 0
+  Chef::Log.warn "No data center found for physical server #{physical_server}"
+  return
+elsif dc_data.length > 1
+  Chef::Application.fatal! "Server #{physical_server} is mentioned in more than one data center!"
+else
+  datacenter = dc_data.first
+  Chef::Log.info "Found server #{physical_server} in data center #{datacenter[:id]}"
+end
+
+#################
+# attributes
+#################
+unless datacenter[:attributes].nil? || datacenter[:attributes].empty?
+  Chef::Log.info "Applying datacenter '#{datacenter[:id]}' attributes: #{datacenter[:attributes].inspect}"
+  # it is important that we merge this with node[:override], as these (normal) attributes would
+  # be kept, if this node would be moved to another DC where this attribute is not set
+  node.override = Chef::Mixin::DeepMerge.deep_merge!(node.override, datacenter[:attributes])
+end
+
+#################
+# cookbooks
+#################
+unless datacenter[:cookbooks].nil?
+  datacenter[:cookbooks].each do |cb|
+    Chef::Log.info "Applying datacenter '#{datacenter[:id]}' cookbook #{cb}"
+    include_recipe cb
+  end
+end
+
+#################
+# label myself to make me searchable
+#################
+node.set[:datacenter] = datacenter[:id]
+
+include_recipe "#{cookbook_name}::_production" if datacenter
--- recipes/_production.rb
+++ recipes/_production.rb
@@ -1,22 +1,33 @@
-node.set[:postfix][:aliases][:root] = %w{
-  michael.stucki@typo3.org
-  peter.niederlag@typo3.org
-  steffen.gebert@typo3.org
-  bastian.bringenberg@typo3.org
-  fabien.udriot@typo3.org
-}
-node.set[:postfix][:aliases][:bbringenberg] = 'bastian.bringenberg@typo3.org'
-node.set[:postfix][:aliases][:mstucki]      = 'michael.stucki@typo3.org'
-node.set[:postfix][:aliases][:sgebert]      = 'steffen.gebert@typo3.org'
-node.set[:postfix][:aliases][:pniederlag]   = 'peter.niederlag@typo3.org'
-node.set[:postfix][:aliases][:fudriot]      = 'fabien.udriot@typo3.org'
+# recipes that should be included, if we are in production
+production_recipes = %w(
+etckeeper::commit
+t3-zabbix::agent
+backuppc
+)
 
-%w{
-  t3-chef-client
-  t3-chef-client::knife-lastrun
-  etckeeper::commit
-  t3-zabbix::agent
-  backuppc
-}.each do |recipe|
+# If we would include t3-chef-client in test-kitchen, this would result in a
+# 401 Unauthorized
+# error when searching the sysadmin data bag in the users_manage("sysadmin") resource.
+# Therefore, exclude t3-chef-client, if we have this magic attribute set
+unless node['t3-base']['prevent-t3-chef-client-inclusion-for-testing']
+  production_recipes << 't3-chef-client'
+  production_recipes << 't3-chef-client::knife-lastrun'
+end
+
+production_recipes.each do |recipe|
   include_recipe recipe
 end
+
+#################
+# postfix aliases
+#################
+
+sysadmins = search("users", "groups:sysadmin AND NOT action:remove")
+
+aliases_root = []
+sysadmins.each do |user|
+  aliases_root << user['email']
+  Chef::Log.info "Set up alias for user #{user['id']}"
+  node.set['postfix']['aliases'][user['id']] = user['email']
+end
+node.set['postfix']['aliases']['root'] = aliases_root
