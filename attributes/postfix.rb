6ea4fb7a875724456ee61e9ca0498aff32c23c26
Michael Stucki <michael.stucki@typo3.org>
2016-06-29 01:02:37 +0200

    [TASK] Fix a typo

--- attributes/postfix.rb
+++ attributes/postfix.rb
@@ -1,4 +1,4 @@
-# wWe do not want <localuser>@srvXXX.typo3.org as sender, as this domain does not exist (and mail server would reject mails)
+# We do not want <localuser>@srvXXX.typo3.org as sender, as this domain does not exist (and mail server would reject mails)
 # Instead, we send mails from <localuser>@typo3.org by specifying the masquerade_domain
 default['postfix']['main']['masquerade_domains'] = (node['domain'] || node['hostname']).to_s.chomp('.')
 
