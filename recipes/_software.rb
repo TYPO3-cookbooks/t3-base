065024036122ef76e6d6b560274a7ed076f1f2e0
Stephan Großberndt <stephan.grossberndt@typo3.org>
2019-05-21 15:57:07 +0200

    [TASK] Add dirmngr for Debian 9

--- recipes/_software.rb
+++ recipes/_software.rb
@@ -23,3 +23,10 @@ packages = %w{
 package packages do
   action :upgrade
 end
+
+# apt-key needs dirmngr which is no longer installed by default on Debian Stretch
+if node[:platform] == "debian" and node[:platform_version].to_i >= 9
+    package "dirmngr" do
+        action :upgrade
+    end
+end
